aa740a7bf9448144e75564eb173659502e4064d8
Alao Zubair Lanre <alaozubair@gmail.com>
2024-10-31 09:19:51 +0100

    catch all

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,16 +1,20 @@
-import { Link } from 'react-router-dom'
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const NotFound = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      navigate('/');
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  }, [navigate]);
   return (
-    <div className="flex items-center justify-center flex-col w-full min-h-screen container">
-      <h1 className=" mt-[60px] text-4xl font-bold">404</h1>
-      <p className="text-gray-500 mt-2 text-lg">Oops! The page you're looking for doesn't exist.</p>
-      <Link
-        to="/"
-        className="mt-5 px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 transition duration-200"
-      >
-        Go back to Home
-      </Link>
+    <div className="flex items-center justify-center flex-col w-full min-h-screen container text-center">
+      <h1 className=" mt-[60px] text-3xl font-bold">404: Oops! This page doesn't exist.</h1>
+      <p className="text-gray-500 mt-2 text-lg">You will be redirected to the home page shortly.</p>
     </div>
   );
 };
